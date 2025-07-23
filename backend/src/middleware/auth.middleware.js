c8545d505262578178bc3f165b06f73b46cda042
edik9 <117977013+edik9@users.noreply.github.com>
2025-07-23 14:15:20 +0200

    create admin routes and controllers

--- backend/src/middleware/auth.middleware.js
+++ backend/src/middleware/auth.middleware.js
@@ -2,6 +2,22 @@ import { clerkClient } from "@clerk/express";
 
 export const protectRoute = async (req, resizeBy, next) => {
   if(!req.auth.userId){
-    res.status(401).json({message: "Unauthorized - you must be logget in"})
+    return res.status(401).json({message: "Unauthorized - you must be logget in"})
+  }
+  next()
+}
+
+export const requireAdmin = async(req, res, next) => {
+  try {
+    const currentUser = await clerkClient.users.getUser(req.auth.userId)
+    const isAdmin = process.env.ADMIN_EMAIL === currentUser.primaryEmailAddress.emailAddress
+
+    if(!isAdmin) {
+      return res.status(403).json({ message: "Unauthorized - you must be admin"})
+    }
+
+    next()
+  } catch (error) {
+    next(error)
   }
 }
